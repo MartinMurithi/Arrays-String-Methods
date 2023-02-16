b1f5c786b22279402faffa187640fc7cbac3efa1
MartinMurithi <wachiramartin442@gmail.com>
2023-02-16 08:57:27 +0300

    Reduce

--- Arrays/Reduce.js
+++ Arrays/Reduce.js
@@ -1,10 +1,20 @@
 
 const shop = [
-    { "Item": "Book", "Price":10 },
-    { "Item": "Cake", "Price":30 },
-    { "Item": "Pen", "Price":20 },
-    { "Item": "Car", "Price":100 },
+    { "Item": "Book", Price:10 },
+    { "Item": "Cake", Price:30 },
+    { "Item": "Pen", Price:20 },
+    { "Item": "Car", Price:100 },
 ]
+
+let totalPrice = shop.reduce((currentTotal, item) => {
+    return item.Price + currentTotal
+}, 0);
+console.log(totalPrice);
+console.log(typeof(shop));
+
+
+
+
 
 // const totalPrice = () => {
 //     let price = 0;
